2a44396a92dbeaf57980f2805247a869faee3893
Elbehwashi0 <abdullah.mohamed.9511@gmail.com>
2025-05-08 20:25:39 +0300

    Helper methods for the app

---
+++ app/app_helpers.py
@@ -0,0 +1,60 @@
+import csv
+import numpy as np
+import pandas as pd
+from utils.data_loader import *
+
+users_df = load_cleaned_users()
+ratings_df = load_cleaned_ratings()
+
+def add_new_user(user_name,password,
+                 user_id = users_df.iloc[-1,0] + 1,
+                 loc = "Unknown", Age = None,
+                 path = r"..\data\processed\users_cleaned.csv",
+                 input_df = users_df):
+
+    """
+    Adding a new user to user's dataset
+    """
+
+    if (user_name in input_df["User-Name"].values) or (user_id in input_df.values):
+        print("User already exists")
+        return
+
+    if len(password) < 4:
+        print("Password must be at least 4 characters")
+        return
+
+    new_user_row = [user_id,user_name,loc,Age,password]
+    with open(path, "a",newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(new_user_row)
+        print("User added successfully")
+
+
+
+
+def add_rating(user_id : int,
+               isbn : str,
+               rating : int,
+               path = r"..\data\processed\ratings_cleaned.csv"):
+
+    """
+    Appending a new rating row to ratings_cleaned.csv
+    """
+
+    user_isbns_df = ratings_df.groupby("User-ID")["ISBN"].unique()
+
+    if (user_id in user_isbns_df) and (isbn in user_isbns_df.loc[user_id]):
+        print("User already rated that book")
+        return
+
+    with open(path, "a",newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([int(user_id),str(isbn),float(rating)])
+        print("Rating added successfully")
+
+
+if __name__ == "__main__":
+
+    add_new_user("abood","ab123")
+    add_rating(278844,"0836236688", 10)
